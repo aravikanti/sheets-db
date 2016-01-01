8388ca51d4f339e0ecd10ff30f3904baf469bbce
Aditya Ravikanti <adityakumarvnit@hotmail.com>
2015-12-31 22:46:15 -0600

    dummy Main for testing functionality

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,9 @@
 module Main where
 
 
-import SheetDB
-import SheetTypes
-import qualified Data.Text                 as T
+import qualified Data.Text  as T
+import           SheetDB
+import           SheetTypes
 -- import           Data.Scientific
 
 -- clientid = "1086783968140-bupmbbqo8chsm2tej5ldgfci496a1lvr.apps.googleusercontent.com"
@@ -16,26 +16,27 @@ clientid = "1086783968140-knfg08qu9onnn5b485veaskt2flr0loa.apps.googleuserconten
 clientsecret ="wRktcD6xif1_z6Xv-RhCatBB"
 
 
-dd :: Row
-dd= [p "name" =:  "papaya",
-         p "category" =: "fruit",
-         p "healthiness" =: "adequate",
-          p "type" =: (3 :: Float)]
+toInsert :: Row
+toInsert= [p "name"        =:  "papaya",
+     p "category"    =:  "fruit",
+     p "healthiness" =:  "adequate",
+     p "type"        =:  (3 :: Float)]
 
 main = do
-    mySheet <- access "1hIEq4AAauzI8INelQRIvgxBhmzX44qAB_1QQpFJQ2Xo" clientid clientsecret
+    mySheet <- access "1hIEq4AAauzI8INelQRIvgxBhmzX44qAB_1QQpFJQ2Xo" "od6" clientid clientsecret
 
     case mySheet of
       Just sheet -> do
-         rows <- find (query ((p "type" ~< (2 :: Float)) ~||~ (p "category" ~= "meat" ) ) sheet)
+         rows <- find (query
+                        ((p "type" ~< (2 :: Float)) ~||~ (p "category" ~= "meat" ) ) sheet)
          print rows
-        --  status <- insert dd sheet
+        --  status <- insert toInsert sheet
         --  print status
         --  return ()
-        --  case rows of
-        --    Just rs -> do
-        --      let upd = (T.pack "id" =: (at (T.pack "id") (last rs) :: T.Text)) : dd
-        --      status <- update sheet upd
-        --      print status
-        --    Nothing -> putStrLn "Nothing_there"
+         case rows of
+           Just rs -> do
+             let upd = (p "id" =: (at (p "id") (last rs) :: T.Text)) : toInsert
+             status <- update sheet upd
+             print status
+           Nothing -> putStrLn "Nothing_there"
       Nothing -> putStrLn "Nothing_here"
